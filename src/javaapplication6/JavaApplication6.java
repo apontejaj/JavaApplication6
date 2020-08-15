a7339c87506046393b75243bbd6db804bf56d5cf
Amilcar Aponte <apontejaj@gmail.com>
2020-08-15 16:05:08 +0100

    Update JavaApplication6.java

--- src/javaapplication6/JavaApplication6.java
+++ src/javaapplication6/JavaApplication6.java
@@ -20,6 +20,20 @@ public class JavaApplication6 {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        Person p1 = new Person("Amilcar", 35, 1.77);
+        p1.run();
+        
+        Person p2 = new Person("Peter", 35, 1.77);
+        p2.run();
+        
+        Dog d1 = new Dog("Fluffy");
+        Dog d2 = new Dog("Fluffy");
+        
+        System.out.println("hello you");
+        System.out.println("hello you");
+        System.out.println("hello you");
+        
+        
 
     }
     
